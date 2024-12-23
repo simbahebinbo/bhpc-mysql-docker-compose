aea7bc96707623e9e98ac720e39916e33a63b2fd
simbabdytech <simba@bdy.tech>
2024-12-23 16:05:46 +0800

    增加表

--- init-db/example.sql
+++ init-db/example.sql
@@ -30,3 +30,74 @@ CREATE TABLE hero
     KEY index_username (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='hero table';
 
+
+DROP TABLE IF EXISTS consumer;
+
+CREATE TABLE consumer
+(
+    id bigint(20) unsigned auto_increment COMMENT 'primary key',
+    username varchar(64) NOT NULL DEFAULT '' COMMENT 'username',
+    password varchar(64) NOT NULL DEFAULT '' COMMENT 'password',
+    role varchar(64) NOT NULL DEFAULT '' COMMENT 'role',
+    PRIMARY KEY (id),
+    UNIQUE KEY unique_username (username),
+    KEY index_username (username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='consumer table';
+
+
+
+DROP TABLE IF EXISTS tbl_role;
+
+CREATE TABLE tbl_role (
+  id varchar(50)  NOT NULL,
+  rolename varchar(500)  DEFAULT NULL,
+  createdby varchar(255)  DEFAULT NULL,
+  createddate datetime DEFAULT NULL,
+  updatedby varchar(255)  DEFAULT NULL,
+  data varchar(500)  DEFAULT NULL,
+  updateddate datetime DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+DROP TABLE IF EXISTS tbl_user;
+
+CREATE TABLE tbl_user (
+  id varchar(50)  NOT NULL,
+  address varchar(500)  DEFAULT NULL,
+  authenticate varchar(500)  DEFAULT NULL,
+  mail varchar(500)  DEFAULT NULL,
+  name varchar(500)  DEFAULT NULL,
+  password varchar(500)  DEFAULT NULL,
+  phone varchar(500)  DEFAULT NULL,
+  sex varchar(500)  DEFAULT NULL,
+  updatedby varchar(500)  DEFAULT NULL,
+  mobile varchar(500)  DEFAULT NULL,
+  updateddate datetime DEFAULT NULL,
+  realname varchar(500)  DEFAULT NULL,
+  dob datetime DEFAULT NULL,
+  logo varchar(500)  DEFAULT NULL,
+  createdby varchar(255)  DEFAULT NULL,
+  createddate datetime DEFAULT NULL,
+  openid varchar(255)  DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS tbl_user_permission;
+
+CREATE TABLE tbl_user_permission (
+  id varchar(50)  NOT NULL,
+  userid varchar(500)  DEFAULT NULL,
+  permission varchar(500)  DEFAULT NULL,
+  createddate datetime DEFAULT NULL,
+  createdby varchar(500)  DEFAULT NULL,
+  updateddate datetime DEFAULT NULL,
+  updatedby varchar(50)  DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
